265dbff3de671ba166b6d612b4374d6908fa4dba
Ashwin-5100 <115422881+Ashwin-5100@users.noreply.github.com>
2025-03-17 18:13:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
+++ Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
@@ -5,37 +5,91 @@ using namespace std;
 
 // } Driver Code Ends
 
+class DSU{
+  vector<int> rank;
+  vector<int> parent;
+  public:
+      DSU(int n)
+      {
+          rank.resize(n+1, 0);
+          parent.resize(n+1);
+          for(int i=0;i<=n;i++)
+          {
+              parent[i] = i;
+          }
+      }
+      
+      int findpar(int node)
+      {
+          if(node == parent[node])
+          {
+              return node;
+          }
+          
+          return parent[node] = findpar(parent[node]);
+      }
+      
+      void unionByrank(int u, int v)
+      {
+          int par_u = findpar(u);
+          int par_v = findpar(v);
+          if(par_u == par_v)
+          {
+              return;
+          }
+          if(rank[par_u] < rank[par_v])
+          {
+              parent[par_u] = par_v;
+          }
+          else if(rank[par_v] < rank[par_u])
+          {
+              parent[par_v] = par_u;
+          }
+          else
+          {
+              parent[par_v] = par_u;
+              rank[par_u]++;
+          }
+      }
+  
+};
+
+
 class Solution {
   public:
     // Function to find sum of weights of edges of the Minimum Spanning Tree.
-    int spanningTree(int V, vector<vector<int>> adj[]) {
+    int spanningTree(int V, vector<vector<int>> edg[]) {
         // code here
-        vector<int> vis(V,0);
-        int ans=0;
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
-        pq.push({0,0});
-        while(!pq.empty())
+        vector<pair<int, pair<int,int>>> adj;
+        for(int i=0;i<V;i++)
         {
-            int node = pq.top().second;
-            int wt = pq.top().first;
-            pq.pop();
-            if(vis[node])
-            {
-                continue;
+            for(auto x : edg[i])
+            {   int wt = x[1];
+                int u = i;
+                int y = x[0];
+                
+                adj.push_back({wt, {u, y}});
             }
-            vis[node] = 1;
-            ans+=wt;
-            for(auto x: adj[node])
+        }
+        sort(adj.begin(), adj.end());
+        
+        DSU d(V);
+        int ans=0;
+        for(auto x : adj)
+        {
+            int u = x.second.first;
+            int wt = x.first;
+            int v = x.second.second;
+            
+            if(d.findpar(u) != d.findpar(v))
             {
-                int adjnode = x[0];
-                int adj_wt = x[1];
-                if(!vis[adjnode])
-                {
-                    pq.push({adj_wt, adjnode});
-                }
+                ans+=wt;
+                d.unionByrank(u,v);
+                
             }
         }
         return ans;
+        
     }
 };
 
